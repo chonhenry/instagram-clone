84119ca009aa96c00fd36183f97205c7fc6cea08
Henry Chon <47991972+chonhenry@users.noreply.github.com>
2020-07-21 16:27:38 -0400

    first commit; app setup; connecting to MongoDB with Mongoose; setup route files with express router

---
+++ config/db.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const config = require("config");
+const db = config.get("mongoURI");
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("MongoDB Connected...");
+  } catch (err) {
+    console.error(err.message);
+    // Exit oricess with failure
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
+
+// const MongoClient = require('mongodb').MongoClient;
+// const uri = "mongodb+srv://henrychondev:<password>@instagram-clone.n1hev.mongodb.net/<dbname>?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, { useNewUrlParser: true });
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
