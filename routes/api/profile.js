1d7a36becaee392c5bf0138570734d6a3e557119
Henry Chon <47991972+chonhenry@users.noreply.github.com>
2020-07-24 19:44:45 -0400

    update user profile route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -27,7 +27,44 @@ router.get("/:username", auth, async (req, res) => {
 });
 
 // @route    PUT api/profile
-// @desc     Update user profile
+// @desc     Edit user profile
+// @access   Private
+router.put(
+  "/",
+  [
+    auth,
+    [
+      check("username", "Username cannot be empty").not().isEmpty(),
+      check("email", "Email cannot be empty").not().isEmpty(),
+      check("name", "Name cannot be empty").not().isEmpty(),
+    ],
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    let { username, email, name, bio, gender } = req.body;
+
+    try {
+      const filter = { _id: req.user.id };
+      const update = { username, email, name, bio, gender };
+
+      let profile = await User.findOneAndUpdate(filter, update, {
+        new: true,
+      });
+
+      return res.json({ profile });
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
+
+// @route    DELETE api/profile
+// @desc     Edit user profile
 // @access   Private
 
 module.exports = router;
