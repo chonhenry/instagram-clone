853e1261af34212528d2ebdba6180ffc1db0d90d
Henry Chon <47991972+chonhenry@users.noreply.github.com>
2020-09-16 08:02:30 -0400

    show images on profile details

--- client/src/components/Profile/PostDetails/LeftSide/LeftSide.js
+++ client/src/components/Profile/PostDetails/LeftSide/LeftSide.js
@@ -1,8 +1,51 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./LeftSide.scss";
 
-const LeftSide = () => {
-  return <div className="left-side">left</div>;
+const LeftSide = ({ images }) => {
+  const [currentImg, setCurrentImg] = useState(0);
+  const [leftArrow, setLeftArrow] = useState(false);
+  const [rightArrow, setRightArrow] = useState(false);
+
+  useEffect(() => {
+    if (currentImg === 0) setLeftArrow(false);
+    else if (currentImg > 0 && images.length > 0) setLeftArrow(true);
+
+    if (currentImg === images.length - 1) setRightArrow(false);
+    else if (currentImg < images.length - 1 && images.length > 0)
+      setRightArrow(true);
+  }, [currentImg]);
+
+  const onArrowClick = (direction) => {
+    if (direction === "left") {
+      setCurrentImg((prevCurrentImg) => prevCurrentImg - 1);
+    } else {
+      setCurrentImg((prevCurrentImg) => prevCurrentImg + 1);
+    }
+  };
+
+  return (
+    <div className="left-side">
+      <img
+        className="post-images"
+        src={`data:image/jpeg;base64,${images[currentImg]}`}
+        alt=""
+      />
+
+      {leftArrow && (
+        <i
+          className="fas fa-chevron-circle-left arrow"
+          onClick={() => onArrowClick("left")}
+        />
+      )}
+
+      {rightArrow && (
+        <i
+          className="fas fa-chevron-circle-right arrow"
+          onClick={() => onArrowClick("right")}
+        />
+      )}
+    </div>
+  );
 };
 
 export default LeftSide;
--- client/src/components/Profile/PostDetails/PostDetails.js
+++ client/src/components/Profile/PostDetails/PostDetails.js
@@ -1,16 +1,23 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import LeftSide from "../PostDetails/LeftSide/LeftSide";
 import RightSide from "../PostDetails/RightSide/RightSide";
 import "./PostDetails.scss";
 
 const PostDetails = ({ post }) => {
+  const [currentImg, setCurrentImg] = useState(0);
+
+
   useEffect(() => {
-    console.log(post);
+    console.log("post.image.length:", post.image.length);
   }, []);
 
   return (
     <div className="profile-details-container">
-      <LeftSide />
+      <LeftSide
+        images={post.image}
+        l_arrow={currentImg !== 0 ? true : false}
+        r_arrow={currentImg !== post.image.length ? true : false}
+      />
       <RightSide />
     </div>
   );
