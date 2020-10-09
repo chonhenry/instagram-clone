4e3d66f0eeec9eed1e1f12cf7c60aa94e7e1ab17
Henry Chon <47991972+chonhenry@users.noreply.github.com>
2020-10-08 22:28:41 -0400

    show latest posts on main page

--- client/src/pages/Main/Main.js
+++ client/src/pages/Main/Main.js
@@ -8,7 +8,7 @@ import "./Main.scss";
 const Main = ({ loading, user }) => {
   const [latestPosts, setLatestPosts] = useState([]);
   const [followingUsers, setFollowingUsers] = useState([]);
-  const [selectedUsers, setSelectedUsers] = useState([]);
+  //const [selectedUsers, setSelectedUsers] = useState([]);
 
   useEffect(() => {
     console.log("useEffect");
@@ -22,50 +22,54 @@ const Main = ({ loading, user }) => {
 
   const getSelectedUsers = async (following) => {
     // randomly pick 5 users from following users
-    let tmp = [];
+    let selectedUsers = [];
     if (following.length > 5) {
       for (let i = 0; i < 5; i++) {
         let idx = Math.floor(Math.random() * following.length);
-        tmp.push(following[idx]);
+        selectedUsers.push(following[idx]);
         following.splice(idx, 1);
       }
     } else {
-      tmp = [...following];
+      selectedUsers = [...following];
       following = [];
     }
 
     // get post from selected users
-    let posts = [];
-    tmp.forEach(async (user) => {
+    selectedUsers.forEach(async (user) => {
       const resUser = await axios.get(`/api/users/${user.username}`);
-      console.log(resUser.data);
+      //console.log(resUser.data);
 
       // find post that has not been shown yet
       let postId = resUser.data.posts.find((id) => {
-        if (id.post !== latestPosts.find((id2) => id2.postId === id.post))
+        //console.log("id", id.post);
+        if (latestPosts.find((id2) => id2.postId === id.post) === undefined)
           return id.post;
       });
 
-      const resPost = await axios.get(`/api/posts/${postId.post}`);
-      setLatestPosts((prev) => [
-        ...prev,
-        {
-          username: resUser.data.username,
-          profileImg: resUser.data.profileImg,
-          postId: postId.post,
-          caption: resPost.data.caption,
-        },
-      ]);
+      if (postId) {
+        const resPost = await axios.get(`/api/posts/${postId.post}`);
+        console.log(resPost.data);
+        setLatestPosts((prev) => [
+          ...prev,
+          {
+            username: resUser.data.username,
+            profileImg: resUser.data.profileImg,
+            postId: postId.post,
+            caption: resPost.data.caption,
+          },
+        ]);
+      }
     });
     setFollowingUsers(following);
   };
 
   const handleScroll = (e) => {
     if (e.target.scrollHeight - e.target.scrollTop === e.target.clientHeight) {
-      if (followingUsers.length > 0) {
-        getSelectedUsers(followingUsers);
-      } else if (followingUsers.length === 0) {
+      if (followingUsers.length === 0) {
         setFollowingUsers([...user.following]);
+        getSelectedUsers([...user.following]);
+      } else {
+        getSelectedUsers(followingUsers);
       }
     }
   };
