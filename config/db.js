d25fadd1b98d454a8fa537a323514957d5ece665
Henry Chon <47991972+chonhenry@users.noreply.github.com>
2020-10-17 12:07:16 -0400

    prepared for heroku deployment

--- config/db.js
+++ config/db.js
@@ -17,12 +17,3 @@ const connectDB = async () => {
 };
 
 module.exports = connectDB;
-
-// const MongoClient = require('mongodb').MongoClient;
-// const uri = "mongodb+srv://henrychondev:<password>@instagram-clone.n1hev.mongodb.net/<dbname>?retryWrites=true&w=majority";
-// const client = new MongoClient(uri, { useNewUrlParser: true });
-// client.connect(err => {
-//   const collection = client.db("test").collection("devices");
-//   // perform actions on the collection object
-//   client.close();
-// });
